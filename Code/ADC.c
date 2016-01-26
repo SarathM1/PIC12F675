577a48689a05ca4e3ff837df7ac2b4f59c51eb5f
Sarath M <sarathm00@gmail.com>
2016-01-27 00:31:03 +0530

    Working

--- Code/ADC.c
+++ Code/ADC.c
@@ -6,50 +6,67 @@
  */
 #include <xc.h>
 #define LED GPIO1
+#define _XTAL_FREQ 4000000
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+
 
 void ADC_init()
 {
-    ADFM = 1;
-    VCFG = 0;
-    CHS1 = 0;
-    CHS0 = 0;
-    ADON = 1;
-    
-    ANSEL = 0X00; // Just clearing all default one's
-    ANS0 = 1;
-    ADCS2 = 1;
-    ADCS0 = 1;
+	ANSEL   = 0xA1;	     // Clear Pin selection bits
+	TRISIO1  = 0;      // GP3 input, rest all output
+	TRISIO0 = 1;	 // Make selected channel pins input
+	ADCON0 = 0x81;		 // Turn on the A/D Converter
+	CMCON  = 0x07;		 // Shut off the Comparator, so that pins are available for ADC
+	VRCON  = 0x00;	     // Shut off the Voltage Reference for Comparator
 }
 
-int ADC_get()
+
+/*
+ * Function Name: GetADCValue
+ * Input(s) :     Channel name, it can be AN0, AN1, AN2 or AN3 only.
+ *                Channel is selected according to the pin you want to use in
+ *                the ADC conversion. For example, use AN0 for GP0 pin.
+ *				  Similarly for GP1 pin use AN1 etc.
+ * Output(s):     10 bit ADC value is read from the pin and returned.
+ * Author:        M.Saeed Yasin   20-06-12
+ */
+unsigned int ADC_get()
 {
-    int value;
-    GO_DONE = 1;
-    while(!ADIF)
-        continue;
-    ADIF = 0;
-    value = (ADRESH<<8) + ADRESL;
-    return value;
+    ADRESH=0x00;
+    ADRESL=0x00;
+    ADCON0 = 0x81;
+    __delay_us(25);
+    
+    GO_nDONE = 1;                //Initializes A/D conversion
+    while(GO_nDONE);             //Waiting for conversion to complete
+    
+    return (ADRESH<<8)+ADRESL;
 }
 void main(void) 
 {
-    TRISIO0 = 1;
-    TRISIO1 = 0;
-    CMCON = 0x07;
-    OPTION_REG = 0X80;
+    GPIO=0x00;
     LED = 0;
     ADC_init();
     
     while(1)
     {
+        __delay_ms(500);
         if(ADC_get())
-        {
-            LED = 1;
-        }
-        else
         {
             LED = 0;
         }
+        else
+        {
+            LED = 1;
+        }
+        
     }
     return;
 }
