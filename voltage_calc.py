a0c0cb1175c5d8d86121914f87fda4705ff612d7
Sarath M <sarathm00@gmail.com>
2016-01-29 23:20:20 +0530

    python code to calculate rms voltage from firing delay

---
+++ voltage_calc.py
@@ -0,0 +1,19 @@
+from math import sin,pi,sqrt,fabs
+
+t = 2					# Give time in ms
+alpha = pi*t
+alpha = alpha /10
+
+x = sin(2*alpha)
+
+y = pi - alpha + (x/2)
+z = y/pi
+z = fabs(z)
+try:
+	vrms = 230 * sqrt(z)
+except Exception, e:
+	vrms = 0
+	print t,alpha,x,y,z
+	print e
+else:
+	print vrms
